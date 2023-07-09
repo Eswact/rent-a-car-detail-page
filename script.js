e075c0315c1df416983e94a5be3786167d14d0ac
Eren Sarıbay <eren99srb@gmail.com>
2023-07-09 15:07:28 +0300

    format-doc

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-$(".mini-img").click(function(){
+$(".mini-img").click(function () {
     var selectedImg = this.dataset.img;
-    $(".mini-img").each(function(){
+    $(".mini-img").each(function () {
         $(this).removeClass("selected");
     });
     $(this).addClass("selected");
-    $(".car-img").each(function(){
-        if(this.dataset.img == selectedImg){
-            $(this).css("display","block");
+    $(".car-img").each(function () {
+        if (this.dataset.img == selectedImg) {
+            $(this).css("display", "block");
         }
-        else{
-            $(this).css("display","none");
+        else {
+            $(this).css("display", "none");
         }
     });
 });
 $(".mini-img")[0].click();
-$(".img-next").click(function(){
+$(".img-next").click(function () {
     var visible = $(".car-img:visible").data("img");
-    if(visible == 4){
+    if (visible == 4) {
         var newVisible = 1
     }
-    else{
+    else {
         var newVisible = ++visible;
     }
-    $(".mini-img[data-img=" + newVisible +"]").click();
+    $(".mini-img[data-img=" + newVisible + "]").click();
 });
-$(".img-back").click(function(){
+$(".img-back").click(function () {
     var visible = $(".car-img:visible").data("img");
-    if(visible == 1){
+    if (visible == 1) {
         var newVisible = 4
     }
-    else{
+    else {
         var newVisible = --visible;
     }
-    $(".mini-img[data-img=" + newVisible +"]").click();
+    $(".mini-img[data-img=" + newVisible + "]").click();
 });
